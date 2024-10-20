3d3202472e9b7de7e77d5274dca0c7fe54bcbf25
ImJustRob <robertalothian@hotmail.com>
2024-10-20 16:58:07 -0400

    added http to gem file and edit index action still broken

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
     def index
-        render json: {message: "hello"}
+        response = HTTP.get("https://newsapi.org/v2/everything?q=tesla&from=2024-09-20&sortBy=publishedAt&apiKey=#
+        {Rails.application.credentials[:news_api_key]}") 
+        p response.parse(:json)
+        render json: response.parse(:json)
     end
 end
